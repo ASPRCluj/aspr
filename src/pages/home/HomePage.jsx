db73bd4316b31b7b5a2b535314eeb774e47d94c6
maraned05 <168206853+maraned05@users.noreply.github.com>
2026-02-08 20:54:45 +0200

    initial first homepage section

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -7,6 +7,16 @@ function HomePage() {
             <div className='header'>
                 <Navbar />
             </div>
+            <div className='body'>
+                <div className='first-section'>
+                    <div className='motto-text'>
+                        <p>Noi pentru <span className='bold-word'>ASPR</span>,<br /><span className='bold-word'>ASPR</span> pentru toți!</p>
+                    </div>
+                    <div className='logo-pic'>
+                        <img src={`${import.meta.env.BASE_URL}logo_white.png`} alt="ASPR-logo" className='logo'/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
